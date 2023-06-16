f5131a76b8e1924a24e3aaa6a0f6429924e26aa5
Ayush Mangal <mangal.ayush.4982@gmail.com>
2023-06-16 23:27:17 +0530

    tower of hanoi and largest word

---
+++ recursion/towerOfHanoi.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+void TOH(int n, char src, char dest, char helper) {
+    if(n==0)
+        return;
+    TOH(n-1, src, helper, dest);
+    cout << "Move from " << src << " to " << dest << endl;
+    TOH(n-1, helper, dest,src);
+}
+int main() {
+    TOH(4,'A','C','B');
+    return 0;
+}
